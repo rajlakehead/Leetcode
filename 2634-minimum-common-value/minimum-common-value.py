bad06685355fa646a3337d0bb7782dbd460def68
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-03-09 12:46:50 -0500

    Time: 338 ms (89.74%) | Memory: 35.7 MB (53.64%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,5 +1,7 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        if nums1[-1] < nums2[0]:
+            return -1
         for n in nums1:
             idx1 = bisect.bisect_left(nums2, n)
             idx2 = bisect.bisect_right(nums2, n)
