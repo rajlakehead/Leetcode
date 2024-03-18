888fdd9f72f2be6650f97a166d941d98f9bbc246
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-03-18 13:05:40 -0400

    Time: 349 ms (32.71%) | Memory: 66.1 MB (29.85%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        alice = 0
+        bob = 0
+        @cache
+        def dp(i, j):
+            if i > j:
+                return 0
+            
+            a1 = piles[i] + max(dp(i + 1, j - 1), dp(i + 2, j))
+            a2 = piles[j] + max(dp(i + 1, j - 1), dp(i, j - 2))
+
+            return max(a1, a2)
+
+
+        
+        alice = dp(0, len(piles) - 1)
+        return sum(piles) - alice < alice
