3ac87c7d3118e3a9e18071d62738aa6c1acc3830
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-04-04 12:56:49 -0400

    Time: 1289 ms (57.25%) | Memory: 33.6 MB (50.76%) - LeetSync

---
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        res = 1
+        left = 0
+        count = defaultdict(int)
+        currmax = 0
+
+        for right in range(len(nums)):
+            count[nums[right]] += 1
+            if count[nums[right]] > currmax:
+                currmax = count[nums[right]]
+
+            while (right - left + 1) - currmax > k:
+                count[nums[left]] -= 1
+                left += 1
+                if count[nums[left]] == 0:
+                    del count[nums[left]]
+            
+            res = max(res, currmax)
+        return res
+                
+
+
+        
