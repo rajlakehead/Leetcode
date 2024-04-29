56300a8007b554e7e664d5edd2410a87c6c43550
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-04-29 13:30:27 -0400

    Time: 56 ms (75.57%) | Memory: 49.8 MB (9.36%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        return new Promise(async (resolve, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+            try{
+                const res = await fn(...args);
+                resolve(res)
+            }
+            catch(error) {
+                reject(error)
+
+            }
+        })
+        
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
