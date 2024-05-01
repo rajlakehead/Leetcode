d4211410fb7c3cf176aed4a1d06965c1de76713b
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-05-01 14:25:45 -0400

    Time: 313 ms (75.10%) | Memory: 90.6 MB (83.90%) - LeetSync

---
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    const result = {};
+
+    // 1. initialization
+    arr1.forEach(item => {
+        result[item.id] = item;
+    });
+    // 2. joining
+    arr2.forEach(item => {
+        if (result[item.id]) {
+            Object.keys(item).forEach(key => {
+                result[item.id][key] = item[key];    
+            });
+        } else {
+            result[item.id] = item;
+        }
+    });
+
+    return Object.values(result);
+    
+};
