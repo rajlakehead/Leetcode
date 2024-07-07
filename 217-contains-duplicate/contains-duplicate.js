a7e0e15777f6b0eae8484a2b08e7643346103483
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-07-07 14:13:05 -0400

    Time: 71 ms (87.19%) | Memory: 62.1 MB (54.22%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let seen = new Set();
+
+    for(const num of nums){
+        if(seen.has(num)){
+            return true;
+        }
+        seen.add(num)
+    }
+
+    return false;
+    
+};
