e8dce618affc01c3139a7da0d822920b0f11b162
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-07-07 14:56:57 -0400

    Time: 136 ms (5.20%) | Memory: 20 MB (61.75%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,11 +3,8 @@ class Solution:
         hashmap = defaultdict(list)
         for word in strs:
             w = "".join(sorted(word))
+            hashmap[w].append(word)
             
-            if w in hashmap:
-                hashmap[w].append(word)
-            else:
-                hashmap[w].append(word)
         return hashmap.values()
             
         
