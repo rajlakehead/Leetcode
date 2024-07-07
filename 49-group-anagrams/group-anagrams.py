b720da3a3dc914732b0bc1a8a2b83b4617e620a7
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-07-07 14:40:09 -0400

    Time: 110 ms (11.16%) | Memory: 20.3 MB (50.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {}
+        hashmap = defaultdict(list)
         for word in strs:
             w = "".join(sorted(word))
             
             if w in hashmap:
                 hashmap[w].append(word)
             else:
-                hashmap[w] = [word]
+                hashmap[w].append(word)
         return hashmap.values()
             
         
