c88d769e3ce5fe36ba512beb77ea6e3bd3546757
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-07-13 10:28:08 -0400

    Time: 65 ms (97.35%) | Memory: 16.6 MB (27.28%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = defaultdict(int)
-        res = float('-inf')
+        resLen, maxFeq = 0, 0
+        counter = defaultdict(int)
+        maxFeqChar = ""
         left = 0
-        currmax = 0
 
         for right in range(len(s)):
-            count[s[right]] += 1
-            currmax = max(currmax, count[s[right]])
+            counter[s[right]] += 1
+            if counter[s[right]] > maxFeq:
+                maxFeq = counter[s[right]]
+                maxFeqChqe = s[right]
 
-            while (right - left + 1) - currmax > k:
-                count[s[left]] -= 1
-                currmax = max(count.values())
+            while (right - left + 1) - maxFeq > k:
+                counter[s[left]] -= 1
+                if s[left] == maxFeqChar:
+                    maxFeq -= 1
+                if counter[s[left]] == 0:
+                    del counter[s[left]]
                 left += 1
-            res = max(res, right - left + 1)
-        return res
+            
+            resLen = max(resLen, (right - left + 1))
+
+        return resLen
+
 
         
