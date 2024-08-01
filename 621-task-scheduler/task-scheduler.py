7d383bbdc95a68c89e905419de74b0d4ecb896b2
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-08-01 12:33:28 -0400

    Time: 510 ms (18.00%) | Memory: 17.1 MB (53.31%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,20 +1,28 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
-        maxheap = [-c for c in count.values()]
-        heapq.heapify(maxheap)
-        time = 0
-        queue = deque()
-
-        while maxheap or queue:
-            time += 1
-
-            if maxheap:
-                cnt = 1 + heapq.heappop(maxheap)
-                if cnt:
-                    queue.append((cnt, time + n))
-            
-            if queue and queue[0][1] <= time:
-                heapq.heappush(maxheap, queue.popleft()[0])
-        return time
+        count = (Counter(tasks))
+        minheap = []
+        for char, val in count.items():
+            minheap.append([-val, char])
         
+        q = collections.deque()
+        heapq.heapify(minheap)
+        currtime = 0
+
+        while minheap or q:
+            currtime += 1
+
+            while q and q[0][0] <= currtime:
+                _, ch, feq = q.popleft()
+                heapq.heappush(minheap, [feq, ch])
+
+            if minheap:
+                freq, t = heapq.heappop(minheap)
+                freq += 1
+
+                if freq != 0:
+                    q.append([currtime + n + 1, t , freq])
+            
+        return currtime
+
+
