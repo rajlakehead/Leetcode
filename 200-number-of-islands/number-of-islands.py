d82a85505c40330dfa13b9a287c0c8cd9dde88f9
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-08-04 14:28:37 -0400

    Time: 234 ms (74.20%) | Memory: 18.9 MB (73.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(r, c):
+        island = 0
 
+        def dfs(r, c):
             if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]):
-                return 
-            if grid[r][c] == '0':
+                return
+            if grid[r][c] == "0":
                 return
             
-            grid[r][c] = '0'
+            grid[r][c] = "0"
 
             dfs(r + 1, c)
             dfs(r, c + 1)
             dfs(r - 1, c)
             dfs(r, c - 1)
         
-        res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
+                if grid[i][j] == "1":
+                    island += 1
                     dfs(i, j)
-                    res += 1
-        return res
+        return island
+
         
