250028df2b87eca61d1bdd7846e6e452830b6fce
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-09-11 09:02:48 -0400

    Time: 55 ms (10.00%) | Memory: 17.7 MB (65.64%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,24 +6,23 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
 
+        ans = True
         def dfs(root):
+            nonlocal ans
             if not root:
                 return 0
-
-            left = dfs(root.left)
-            right = dfs(root.right)
-
-            if left == -1 or right == -1 or abs(left - right) > 1:
-                return -1
             
-            return 1 + max(left, right)
+            left = 1 + dfs(root.left) if root.left else 0
+            right = 1 + dfs(root.right) if root.right else 0
 
-        
-        return dfs(root)!= -1
+            if abs(left - right) > 1:
+                ans = False
 
-
-
+            return max(left, right)
             
+        dfs(root)
+        return ans
 
-        
