1b3e313c0da4916094d4cdb0ff565799b795d718
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2024-09-19 13:24:51 -0400

    Time: 62 ms (57.66%) | Memory: 16.6 MB (58.92%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,17 +4,21 @@ class Solution:
         left = 0
         length = len(count)
 
+
+
         for right in range(len(s2)):
             count[s2[right]] -= 1
+
             if count[s2[right]] == 0:
                 length -= 1
             
             if (right - left + 1) == len(s1):
                 if length == 0:
                     return True
+                
                 count[s2[left]] += 1
                 if count[s2[left]] == 1:
                     length += 1
+
                 left += 1
         return False
-            
