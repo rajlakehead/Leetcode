7d31cba2361e25034f95a984ab6b4e41fc0c90e7
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2025-02-02 11:24:32 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (45.92%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,18 +1,19 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        hashmap = defaultdict(list)
-        sorted_trips = sorted(trips, key=lambda x: x[1])
-        last_stop = 0
-        start = sorted_trips[0][1]
-        for t in sorted_trips:
-            hashmap[t[1]].append(t[0])
-            hashmap[t[2]].append(-t[0])
-            last_stop = max(last_stop, t[1], t[2])
-        for i in range(start, last_stop + 1):
-            capacity -= sum(hashmap[i])
-            if capacity < 0:
+        points = []
+
+        for Pass, start, end in trips:
+            points.append([start, Pass])
+            points.append([end, -Pass])
+        
+        points.sort()
+        currPass = 0
+
+        for _, count in points:
+            currPass += count
+
+            if currPass > capacity:
                 return False
+        
         return True
-
-
-
+        
