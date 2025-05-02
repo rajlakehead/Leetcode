03d34667cdceadfefea14f3b093843b0978fcd24
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2025-05-02 15:50:33 -0400

    Time: 3 ms (77.07%) | Memory: 19.7 MB (24.28%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,34 +1,32 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        preMap = {}
-        visit = set()
-        path = set()
+        adj = defaultdict(list)
+
+        for src, dst in prerequisites:
+            adj[src].append(dst)
+        
+        visit = [0] * (numCourses)
+        has_cycle = False
         res = []
 
+        def dfs(src):
+            nonlocal has_cycle
+            if visit[src] == 1:
+                has_cycle = True
+                return
+            
+            if visit[src] == 2:
+                return
+
+            visit[src] = 1
+
+            for neighbour in adj[src]:
+                dfs(neighbour)
+            res.append(src)
+            visit[src] = 2
+             
         for i in range(numCourses):
-            preMap[i] = []
-        
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
-        
-        def dfs(crs):
-            if crs in visit:
-                return True
-            if crs in path:
-                return False
-
-            path.add(crs)
-
-            for pre in preMap[crs]:
-                if not dfs(pre): return False
-
-            visit.add(crs)
-            res.append(crs)
-            path.remove(crs)
-
-            return True
-
-        
-        for crs in range(numCourses):
-            if not dfs(crs): return []
+            dfs(i)
+            if has_cycle:
+                return []
         return res
