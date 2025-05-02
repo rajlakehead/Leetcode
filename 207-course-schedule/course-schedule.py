b7b4638cacd6f16048d7283789eb581d94d41455
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2025-05-02 15:20:39 -0400

    Time: 7 ms (47.64%) | Memory: 19.7 MB (13.89%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,34 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preReqMap = {}
+        adj = defaultdict(list)
 
+        for src, dst in prerequisites:
+            adj[src].append(dst)
+        
+        visit = [0] * (numCourses)
+        has_cycle = False
+        print(adj)
+
+        def dfs(src):
+            nonlocal has_cycle
+            if visit[src] == 1:
+                has_cycle = True
+                return
+            
+            if visit[src] == 2:
+                return
+
+            visit[src] = 1
+
+            for neighbour in adj[src]:
+                dfs(neighbour)
+            
+            visit[src] = 2
+             
         for i in range(numCourses):
-            preReqMap[i] = []
-
-        for crs, pre in prerequisites:
-            preReqMap[crs].append(pre)
-        
-        visit = set()
-        
-        def dfs(crs):
-            if preReqMap[crs] == []:
-                return True
-            
-            if crs in visit:
-                return False
-
-            visit.add(crs)
-
-            for pre in preReqMap[crs]:
-                if not dfs(pre):
-                    return False
-            
-            preReqMap[crs] = []
-            visit.remove(crs)
-
-            return True
-        
-        for crs in range(numCourses):
-            if not dfs(crs):
+            dfs(i)
+            if has_cycle:
                 return False
         return True
