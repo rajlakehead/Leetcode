1e4669ae25dab7f1627407e128edada7374ce332
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2025-08-07 13:11:35 -0400

    Time: 69 ms (48.56%) | Memory: 20.6 MB (44.72%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,39 @@
+class Solution:
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        adjList = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for edge in prerequisites:
+            adjList[edge[0]].append(edge[1])
+            indegree[edge[1]] += 1
+
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        nodePrerequisites = defaultdict(set)
+
+        while q:
+            node = q.popleft()
+
+            for adj in adjList[node]:
+                # Add node and prerequisite of the node to the prerequisites of adj
+                nodePrerequisites[adj].add(node)
+                for prereq in nodePrerequisites[node]:
+                    nodePrerequisites[adj].add(prereq)
+
+                indegree[adj] -= 1
+                if indegree[adj] == 0:
+                    q.append(adj)
+
+        answer = []
+        for q in queries:
+            answer.append(q[0] in nodePrerequisites[q[1]])
+
+        return answer
