a10fb41ed58ce7ce3c3bd12c0de1904b639c1838
Raj Patel <68782547+rajlakehead@users.noreply.github.com>
2025-08-22 13:59:41 -0400

    Time: 7 ms (50.00%) | Memory: 17.7 MB (91.67%) - LeetSync

--- 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.py
+++ 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.py
@@ -2,15 +2,17 @@ class Solution:
     def getSmallestString(self, s: str, k: int) -> str:
         res = ""
 
-        for char in s:
-            dist = ord(char) - ord('a')
-            dist = min(dist, 26 - dist)
+        for i, char in enumerate(s):
+            dist1 = ord(char) - ord('a')
+            dist2 = ord('z') - ord(char) + 1
+
+            dist = min(dist1, dist2)
 
             if dist <= k:
                 res += 'a'
                 k -= dist
             else:
                 res += chr(ord(char) - k)
-                k = 0
-        return res
+                break
+        return res + s[i + 1:]
         
